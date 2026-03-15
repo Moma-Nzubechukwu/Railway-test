288cad54440672fee7b5f3bb6598d7616ac5847c
shadows's phone <momafransisco@gmail.com>
2026-03-15 18:59:35 +0100

    added shadows dev path to the back end

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/claude-project', (req, res) => {
+ const filePath = 'shadow-52-week-roadmap-1.html'
+    // __dirname ensures the path is relative to your project root
+    res.sendFile(path.join(__dirname, filePath));
+});
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello World' });
 });
-
+app.listen(8080, '0.0.0.0', ()=>{
+	console.log(`server started on port 8080 on http://127.0.0.1:8080`)
+})
 module.exports = app;
